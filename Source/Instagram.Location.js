c7036c781937328fd7f46849248b6d245e18f8cf
Christopher Beloch <cbeloch@me.com>
2011-06-19 21:55:04 +0200

    added Instagram.Location

---
+++ Source/Instagram.Location.js
@@ -0,0 +1,25 @@
+Instagram.implement({
+	getLocation: function(location_id) {
+		this.request('locations/' + location_id, 'locationData');
+	},
+	
+	getLocationMedia: function(location_id, min_time, max_time) {
+		var parameter = new Object();
+
+		if(min_time) { parameter.min_timestamp = min_time; }
+		if(max_time) { parameter.max_timestamp = max_time; }
+
+		this.request('locations/' + location_id + '/media/recent', 'mediaData', parameter);
+	},
+	
+	searchLocation: function(lat, lng, foursquare_id, distance) {
+		var parameter = new Object();
+
+		if(lat) { parameter.lat = lat; }
+		if(lng) { parameter.lng = lng; }
+		if(foursquare_id) { parameter.foursquare_id = foursquare_id; }
+		if(distance) { parameter.distance = distance; }
+
+		this.request('locations/search', 'locationSearch', parameter);
+	}
+});
