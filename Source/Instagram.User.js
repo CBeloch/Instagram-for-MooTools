8f22dc38beab36f198b029610fd2b10bf9e9db6f
Christopher Beloch <cbeloch@me.com>
2011-06-19 16:37:48 +0200

    extended Instagram.User

--- Source/Instagram.User.js
+++ Source/Instagram.User.js
@@ -2,6 +2,24 @@ Instagram.implement({
 	getUser: function(user_id) {
 		user_id = user_id || 'self';
 		
-		this.request('users/' + user_id + '/', 'singeUser', null, true);
+		this.request('users/' + user_id, 'singleUser', null, true);
+	},
+	
+	getFeed: function(count) {
+		var parameter = new Object();
+		if(count) { parameter.count = count; }
+
+		this.request('users/self/feed', 'userFeed', parameter, true);
+	},
+	
+	getUserMedia: function(user_id, count, min_time, max_time) {
+		user_id = user_id || 'self';
+
+		var parameter = new Object();
+		if(count) { parameter.count = count; }
+		if(min_time) { parameter.min_timestamp = min_time; }
+		if(max_time) { parameter.max_timestamp = max_time; }
+
+		this.request('users/' + user_id + '/media/recent', 'mediaData', parameter, true);
 	}
 });
